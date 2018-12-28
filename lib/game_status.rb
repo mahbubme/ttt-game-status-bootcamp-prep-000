58df1572e14cb8a33e8a456488a2ea6fb8545648
Mahbubur Rahman <mail@mahbub.me>
2018-12-28 13:05:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,11 +24,11 @@ def full? ( board )
 end
 
 def draw? ( board )
-  full? ( board ) && !won?( board )
+  full?( board ) && !won?( board )
 end
 
 def over? ( board )
-  
+  won?(board) || draw?(board) || full?(board)
 end
 
 def winner ( board )
