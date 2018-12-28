b93218965fb54a14068854c98256f0be1ad77b7f
Mahbubur Rahman <mail@mahbub.me>
2018-12-28 12:55:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,7 +30,7 @@ def won?( board )
     end
   end
   WIN_COMBINATIONS.detect do |win_combination|
-    board[win_combination[0]] == board[win_combination[1]]
+    board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && position_taken( board, win_combination[0])
   end 
 end
 
